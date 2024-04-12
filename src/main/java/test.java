11678cc0f95fea95d1088c40f4adfe84d3f671c6
“s8x” <xbc20030512@163.com>
2024-04-12 23:07:21 +0800

    1

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,7 +2,5 @@ import java.io.FileInputStream;
 
 public class test {
     public static void main(String[] args) throws Exception {
-        FileInputStream fileInputStream = new FileInputStream("src//1.txt");
-        System.out.println(fileInputStream.read());
     }
 }
